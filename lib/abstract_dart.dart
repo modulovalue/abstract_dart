e2852b28408328bae8814e78b8012da4f08c72b0
Modestas Valauskas <valauskasmodestas@gmail.com>
2019-08-17 12:21:15 +0200

    fixed import

--- lib/abstract_dart.dart
+++ lib/abstract_dart.dart
@@ -5,3 +5,4 @@ export 'src/implementation/fields.dart';
 export 'src/implementation/groups.dart';
 export 'src/implementation/monoids.dart';
 export 'src/implementation/semigroups.dart';
+export 'src/uses/lerp.dart';
