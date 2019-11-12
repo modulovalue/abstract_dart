67c8e1a1052982f7def8798d4086c9b392e1ccdc
Modestas Valauskas <valauskasmodestas@gmail.com>
2019-11-12 15:02:30 +0100

    missing const constructor test added

--- test/implementations/scalar/vector_spaces_test.dart
+++ test/implementations/scalar/vector_spaces_test.dart
@@ -1,7 +1,16 @@
 import 'package:abstract_dart/abstract_dart.dart';
 import 'package:test/test.dart';
 
+// so coverage catches constructors
+//ignore_for_file: prefer_const_constructors
 void main() {
+  group("$DurationScalarMonoid", () {
+    test("scalar", () {
+      expect(DurationScalarMonoid().sIdentity(), 1.0);
+      expect(DurationScalarMonoid().sOperate(_ms300, 1.5),
+          const Duration(milliseconds: 450));
+    });
+  });
   group("$DurationVectorSpace", () {
     test("addition", () {
       expect(DurationVectorSpace().addition.operate(_ms300, _ms700),
@@ -13,8 +22,6 @@ void main() {
     });
     test("scalar", () {
       expect(DurationVectorSpace().scalar.sIdentity(), 1.0);
-      expect(DurationVectorSpace().scalar.sOperate(_ms300, 3),
-          const Duration(milliseconds: 900));
       expect(DurationVectorSpace().scalar.sOperate(_ms300, 1.5),
           const Duration(milliseconds: 450));
     });
